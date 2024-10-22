be0c074ecc9c723678064f1bb2d00c2e3dad4eb7
Reddi Nithin <99125721+nithin17298@users.noreply.github.com>
2024-10-22 17:32:28 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic=[
+    {
+        name: "Pushpa Pushpa",
+        artist:"DSP",
+        img:"music-1",
+        src:"music-1",
+
+    },
+    {
+        name: "Ikson Anywhere- Ikson",
+        artist:"Audio Library",
+        img:"music-2",
+        src:"music-2",
+
+    },
+    {
+        name: "Ticket Eh Konakunda",
+        artist:"Ram Miriyala",
+        img:"music-3",
+        src:"music-3",
+
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist:"Mike Archangelo",
+        img:"music-4",
+        src:"music-4",
+
+    },
+    {
+        name: "Kurchi Madathapetti",
+        artist:"Thaman",
+        img:"music-5",
+        src:"music-5",
+
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist:"NCS Release",
+        img:"music-6",
+        src:"music-6",
+
+    }
+
+]
